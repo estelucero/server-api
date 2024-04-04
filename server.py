a078f64d4cc44378828088b9865b29c135ffdc73
Esteban Lucero <este.lucero2212@gmail.com>
2024-04-04 11:06:30 -0300

    aa

--- server.py
+++ server.py
@@ -1,42 +1,9 @@
-from flask import Flask, jsonify, request
-
-from chatbot import answer_message
-from get_ip import get_local_ip
-
-app = Flask(__name__)
-count = 0
-
-@app.route('/test', methods=['GET'])
-def test():
-  global count
-  count += 1
-  data = {
-    "apiCallsCount": count,
-    "answer" : "ok"
-  }
-  return jsonify(data)
-
-# Contador para llevar registro de las llamadas al endpoint
-# Define el endpoint "/message"
-@app.route('/message', methods=['GET'])
-def message():
-  global count
-  message = request.args.get("message")
-  count += 1
-  data = {
-    "apiCallsCount": count,
-    "answer" : answer_message(message)
-  } 
-  return jsonify(data)
+@app.route('/api/chatbot', methods=['POST'])
+def chatbot():
+    data = request.get_json()
+    user_message = data['message']
+    bot_response = answer_message(user_message)
+    return jsonify({'response': bot_response})
 
 if __name__ == '__main__':
-    # host = get_local_ip()
-    # user_input = input("Ingrese ip (enter for default " + host + "): ")
-    # if (user_input != ""):
-    #   host = user_input
-
-    # user_input = input("Ingrese port (enter for default 5000): ")
-    # port = int(user_input) if user_input != "" else 5000
-
-#    app.run(host, port)
-    app.run()
+    app.run(debug=True)
