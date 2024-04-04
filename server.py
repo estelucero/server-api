73dfe8b9deec97c3c316024af983d63f7d35c7b8
Esteban Lucero <este.lucero2212@gmail.com>
2024-04-04 12:49:11 -0300

    cambio

--- server.py
+++ server.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify, request
-from chatbot import answer_message
-
-app = Flask(__name__)
-
-@app.route('/chatbot', methods=['POST'])
-def chatbot():
-    data = request.get_json()
-    user_message = data['message']
-    bot_response = answer_message(user_message)
-    return jsonify({'response': bot_response})
-
-if __name__ == '__main__':
-    app.run()
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
